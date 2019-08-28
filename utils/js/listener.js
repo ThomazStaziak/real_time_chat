dc0192f15596bfe3fcb064d6b24bab5a42d54ad4
ThomazStaziak <thostaziak@gmail.com>
2019-08-28 17:18:34 -0300

    modifying the html to insert

--- utils/js/listener.js
+++ utils/js/listener.js
@@ -27,14 +27,19 @@ $("document").ready(function() {
 
   const channel = pusher.subscribe("channel");
   channel.bind("chat", function(data) {
+    console.log(data);
+    const chat = data[0];
+    const className = data[1];
     $("#screen").append(
-      `<div class="message">
+      `<div class="${className} message">
           <div class="row w-100 d-flex justify-content-between p-2 m-0">
-            <span><b>${data.username}</b></span>
-            <span>${data.time}</span>
+            <span><b>${chat.username}</b></span>
+            <span>${chat.time.slice(0, 5)}</span>
           </div>
-          <p class="p-2">${data.message}</p>
+          <p class="p-2">${chat.message}</p>
         </div>`
     );
+
+    scrollDown("#screen", 100);
   });
 });
