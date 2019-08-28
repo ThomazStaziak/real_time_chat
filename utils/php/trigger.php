a28f07cf90ac9946d0ce417f556dcec4ad1a5e6a
ThomazStaziak <thostaziak@gmail.com>
2019-08-28 17:14:58 -0300

    making tasks to add the messages in database and socket

--- utils/php/trigger.php
+++ utils/php/trigger.php
@@ -1,5 +1,7 @@
 <?php
+  session_start();
   require_once '../../vendor/autoload.php';
+  require_once 'db_functions.php';
 
   $app_id = '846919';
   $app_key = '2fb071cffdac6f3ff0d7';
@@ -15,11 +17,15 @@
     $app_id, 
     $options
   );
+  
+  insert($_POST['username'], $_POST['message'], date('H:i', time()));
+  $messages = getMessages();
+  $message = end($messages);
 
-  $data['username'] = $_POST['username'];
-  $data['message'] = $_POST['message'];
-  $data['time'] = date('h:i', time());
-
-  $pusher->trigger('channel', 'chat', $data);
+  $className = $_SESSION['username'] == $_POST['username']
+  ? 'mine'
+  : 'yours';
+  
+  $pusher->trigger('channel', 'chat', [$message, $className]);
 ?>
   
