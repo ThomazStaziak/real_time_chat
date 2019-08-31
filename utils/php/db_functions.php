69d63c1ac5b3403b4bba3fb59741d1627eabed61
ThomazStaziak <thostaziak@gmail.com>
2019-08-31 08:39:13 -0300

    adding new functions

--- utils/php/db_functions.php
+++ utils/php/db_functions.php
@@ -1,7 +1,7 @@
 <?php
   $conn = new PDO('pgsql:host=localhost;port=5432;dbname=chat_php;user=postgres;password=docker');
   
-  function insert($username, $message, $time) {
+  function insertChat($username, $message, $time) {
     try {
       global $conn;
 
@@ -18,6 +18,21 @@
     }
   }
 
+  function insertOnlineUser($username) {
+    try {
+      global $conn;
+
+      $insert = $conn->prepare("INSERT INTO online_users (username) VALUES (:username)");
+      $inserted = $insert->execute([
+        ':username' => $username
+      ]);
+
+      return $inserted;
+    } catch (PDOException $err) {
+      echo $err->getMessage();
+    }
+  }
+
   function getMessages() {
     try {
       global $conn;
@@ -26,6 +41,50 @@
       $messages = $select->fetchAll(PDO::FETCH_ASSOC);
   
       return $messages;
+    } catch (PDOException $err) {
+      echo $err->getMessage();
+    }
+  }
+
+  function getOnlineUsers() {
+    try {
+      global $conn;
+
+      $select = $conn->query("SELECT * FROM online_users");
+      $users = $select->fetchAll(PDO::FETCH_ASSOC);
+  
+      return $users;
+    } catch (PDOException $err) {
+      echo $err->getMessage();
+    }
+  }
+
+  function getOnlineUserByName($username) {
+    try {
+      global $conn;
+
+      $select = $conn->prepare("SELECT * FROM online_users WHERE username = :username");
+      $select->execute([
+        ':username' => $username
+      ]);
+      $user = $select->fetch(PDO::FETCH_ASSOC);
+  
+      return $user;
+    } catch (PDOException $err) {
+      echo $err->getMessage();
+    }
+  }
+
+  function deleteOnlineUser($id) {
+    try {
+      global $conn;
+
+      $delete = $conn->prepare("DELETE FROM online_users WHERE id = :id");
+      $deleted = $delete->execute([
+        ':id' => $id
+      ]);
+  
+      return $deleted;
     } catch (PDOException $err) {
       echo $err->getMessage();
     }
